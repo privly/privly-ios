de542b2256c07519ad211f1f70ff571818c957e5
Hery <aeonzamaek@gmail.com>
2013-06-05 15:03:06 -0400

    cleaning

---
+++ privly-ios/TestPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestPostViewController.h
+//  privly-ios
+//
+//  Created by Hery Ratsimihah on 6/5/13.
+//  Copyright (c) 2013 Hery Ratsimihah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestPostViewController : UIViewController
+
+@end
---
+++ privly-ios/TestPostViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestPostViewController.m
+//  privly-ios
+//
+//  Created by Hery Ratsimihah on 6/5/13.
+//  Copyright (c) 2013 Hery Ratsimihah. All rights reserved.
+//
+
+#import "TestPostViewController.h"
+
+@interface TestPostViewController ()
+
+@end
+
+@implementation TestPostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
