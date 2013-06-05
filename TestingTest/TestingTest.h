3f2e44bbe269e29940f1a6a04e2c1f1458f16313
Hery <aeonzamaek@gmail.com>
2013-06-05 13:47:43 -0400

    removed ZeroBin app

---
+++ TestingTest/TestingTest.h
@@ -0,0 +1,13 @@
+//
+//  TestingTest.h
+//  TestingTest
+//
+//  Created by Hery Ratsimihah on 6/1/13.
+//  Copyright (c) 2013 Hery Ratsimihah. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestingTest : SenTestCase
+
+@end
---
+++ TestingTest/TestingTest.m
@@ -0,0 +1,32 @@
+//
+//  TestingTest.m
+//  TestingTest
+//
+//  Created by Hery Ratsimihah on 6/1/13.
+//  Copyright (c) 2013 Hery Ratsimihah. All rights reserved.
+//
+
+#import "TestingTest.h"
+
+@implementation TestingTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestingTest");
+}
+
+@end
