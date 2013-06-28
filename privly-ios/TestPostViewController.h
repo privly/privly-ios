41e4bd4dbbf4571f795dadca752cc51b97c69465
Hery <aeonzamaek@gmail.com>
2013-06-28 15:36:59 -0400

    added support for privly-applications

---
+++ privly-ios/TestPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestPostViewController.h
+//  privly-ios
+//  Copyright 2013 The Privly Foundation.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestPostViewController : UIViewController <UIWebViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIWebView *testPostWebView;
+
+@end
