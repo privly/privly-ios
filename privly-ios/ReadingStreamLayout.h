333f8e8cbc5bec926f60b09ade1334cce8f265df
Hery <aeonzamaek@gmail.com>
2013-08-14 11:03:44 +0200

    added reading stream layout setup

--- privly-ios/ReadingStreamLayout.h
+++ privly-ios/ReadingStreamLayout.h
@@ -8,4 +8,9 @@
 
 @interface ReadingStreamLayout : UICollectionViewLayout
 
+@property (nonatomic) UIEdgeInsets itemInsets;
+@property (nonatomic) CGSize itemSize;
+@property (nonatomic) CGFloat interItemSpacingY;
+@property (nonatomic) NSInteger numberOfColumns;
+
 @end
