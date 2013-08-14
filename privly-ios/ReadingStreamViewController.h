ae42074c78bfb9b257a321ee4a4783c790dca66d
Hery <aeonzamaek@gmail.com>
2013-08-14 12:23:04 +0200

    added tableView with hard-coded list of posts URLs

---
+++ privly-ios/ReadingStreamViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReadingStreamViewController.h
+//  privly-ios
+//  Copyright 2013 The Privly Foundation.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReadingStreamViewController : UITableViewController {
+    NSArray *URLList;
+}
+
+@end
