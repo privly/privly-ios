6c9e055f4b67eeede53a7b598cf2095a6eaeb29b
Hery <aeonzamaek@gmail.com>
2013-08-30 17:09:29 -0400

    added tests for custom navigation controller

---
+++ privly-ios-tests/CustomNavigationViewControllerTest.h
@@ -0,0 +1,15 @@
+//
+//  CustomNavigationViewControllerTest.h
+//  privly-ios
+//  Copyright 2013 The Privly Foundation.
+//
+
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "CustomNavigationViewController.h"
+
+@interface CustomNavigationViewControllerTest : SenTestCase {
+    CustomNavigationViewController *customNavigationViewController;
+}
+
+@end
--- privly-ios-tests/LoginViewControllerTests.h
+++ privly-ios-tests/LoginViewControllerTests.h
@@ -14,5 +14,4 @@
     LoginViewController *loginViewController;
 }
 
-
 @end
